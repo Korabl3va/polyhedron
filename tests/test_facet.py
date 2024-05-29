8489dc4eae60c0ba11eea456785c2ec3dd7b9ee5
Korabl3va <93127100+Korabl3va@users.noreply.github.com>
2024-05-29 23:35:15 +0300

    polyhedron_42

---
+++ tests/test_facet.py
@@ -0,0 +1,112 @@
+import unittest
+from math import sqrt, isclose
+from common.r3 import R3
+from shadow.polyedr import Facet
+from tests.matchers import R3ApproxMatcher, R3CollinearMatcher
+
+
+class TestVoid(unittest.TestCase):
+
+    # Эта грань не является вертикальной
+    def test_vertical01(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)])
+        self.assertFalse(f.is_vertical())
+
+    # Эта грань вертикальна
+    def test_vertical02(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 0.0, 1.0), R3(1.0, 0.0, 0.0)])
+        self.assertTrue(f.is_vertical())
+
+    # Нормаль к этой грани направлена вертикально вверх
+    def test_h_normal01(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)])
+        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(0.0, 0.0, 1.0))
+
+    # Нормаль к этой грани тоже направлена вертикально вверх
+    def test_h_normal02(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 3.0, 0.0), R3(3.0, 0.0, 0.0)])
+        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(0.0, 0.0, 1.0))
+
+    # Для нахождения нормали к этой грани рекомендуется нарисовать картинку
+    def test_h_normal03(self):
+        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0), R3(0.0, 0.0, 1.0)])
+        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(1.0, 1.0, 1.0))
+
+    # Для каждой из следующих граней сначала «вручную» находятся
+    # внешние нормали к вертикальным плоскостям, проходящим через
+    # рёбра заданной грани, а затем проверяется, что эти нормали
+    # имеют то же направление, что и вычисляемые методом v_normals
+
+    # Нормали для треугольной грани
+    def test_v_normal01(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)])
+        normals = [R3(-1.0, 0.0, 0.0), R3(0.0, -1.0, 0.0), R3(1.0, 1.0, 0.0)]
+        for t in zip(f.v_normals(), normals):
+            self.assertEqual(R3CollinearMatcher(t[0]), t[1])
+
+    # Нормали для квадратной грани
+    def test_v_normal02(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(2.0, 0.0, 0.0),
+                   R3(2.0, 2.0, 0.0), R3(0.0, 2.0, 0.0)])
+        normals = [R3(-1.0, 0.0, 0.0), R3(0.0, -1.0, 0.0),
+                   R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0)]
+        for t in zip(f.v_normals(), normals):
+            self.assertEqual(R3CollinearMatcher(t[0]), t[1])
+
+    # Нормали для ещё одной треугольной грани
+    def test_v_normal03(self):
+        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0), R3(0.0, 0.0, 1.0)])
+        normals = [R3(0.0, -1.0, 0.0), R3(1.0, 1.0, 0.0), R3(-1.0, 0.0, 0.0)]
+        for t in zip(f.v_normals(), normals):
+            self.assertEqual(R3CollinearMatcher(t[0]), t[1])
+
+    # Центр квадрата
+    def test_center01(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(2.0, 0.0, 0.0),
+                   R3(2.0, 2.0, 0.0), R3(0.0, 2.0, 0.0)])
+        self.assertEqual(R3ApproxMatcher(f.center()), (R3(1.0, 1.0, 0.0)))
+
+    # Центр треугольника
+    def test_center02(self):
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)])
+        self.assertEqual(R3ApproxMatcher(f.center()), (R3(1.0, 1.0, 0.0)))
+
+    # Центр грани внутри куба
+    def test_cube_center01(self):
+        f = Facet([R3(-0.5, -0.5, 0.0), R3(-0.5, 0.5, 0.0),
+                   R3(0.5, -0.5, 0.0), R3(0.5, 0.5, 0.0)])
+        self.assertEqual(f.is_outside(), False)
+
+    # Центр грани снаружи куба
+    def test_cube_center02(self):
+        f = Facet([R3(10.0, 5.0, 0.0), R3(10.0, 10.0, 5.0),
+                   R3(5.0, 10.0, 5.0), R3(5.0, 5.0, 0.0)])
+        self.assertEqual(f.is_outside(), True)
+
+    # Угол больше pi\7
+    def test_is_less01(self):
+        f = Facet([R3(10.0, 5.0, 0.0), R3(10.0, 10.0, 5.0),
+                   R3(5.0, 10.0, 5.0), R3(5.0, 5.0, 0.0)])
+        self.assertEqual(f.is_less(), False)
+
+    # Угол меньше pi\7
+    def test_is_less02(self):
+        f = Facet([R3(10.0, 5.0, 0.0), R3(10.0, 10.0, 0.0),
+                   R3(5.0, 10.0, 0.0), R3(5.0, 5.0, 0.0)])
+        self.assertEqual(f.is_less(), True)
+
+    # Грань невидима
+    def test_visible01(self):
+        f = Facet([R3(1.0, 1.0, 3.0), R3(6.0, 1.0, 3.0),
+                   R3(6.0, 6.0, 3.0), R3(1.0, 6.0, 3.0)])
+        f_over = f = Facet([R3(0.0, 0.0, 0.0), R3(5.0, 0.0, 0.0),
+                            R3(5.0, 5.0, 0.0), R3(0.0, 5.0, 0.0)])
+        self.assertEqual(f.facet_is_visible([f, f_over]), True)
+
+    # Грань невидима
+    def test_visible02(self):
+        f_over = Facet([R3(1.0, 1.0, 3.0), R3(6.0, 1.0, 3.0),
+                        R3(6.0, 6.0, 3.0), R3(1.0, 6.0, 3.0)])
+        f = f = Facet([R3(0.0, 0.0, 0.0), R3(5.0, 0.0, 0.0),
+                       R3(5.0, 5.0, 0.0), R3(0.0, 5.0, 0.0)])
+        self.assertEqual(f.facet_is_visible([f, f_over]), False)
---
+++ tests/test_polyedr.py
@@ -0,0 +1,195 @@
+import unittest
+from unittest.mock import patch, mock_open
+
+from shadow.polyedr import Polyedr
+
+
+class TestPolyedr1(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """200.0	45.0	45.0	30.0
+8	4	16
+-0.5	-0.5	0.5
+-0.5	0.5	0.5
+0.5	0.5	0.5
+0.5	-0.5	0.5
+-0.5	-0.5	-0.5
+-0.5	0.5	-0.5
+0.5	0.5	-0.5
+0.5	-0.5	-0.5
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5"""
+        fake_file_path = 'data/holey_box1.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 4)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 16)
+
+    def test_area(self):
+        self.polyedr.result()
+        self.assertAlmostEqual(self.polyedr.area, 0.0)
+
+
+class TestPolyedr2(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """200.0	45.0	45.0	30.0
+8	6	24
+0.5	0.5	1.5
+0.5	1.5	1.5
+1.5	1.5	1.5
+1.5	0.5	1.5
+0.5	0.5	0.5
+0.5	1.5	0.5
+1.5	1.5	0.5
+1.5	0.5	0.5
+4	1    2    3    4
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5
+4	8    7    6    5"""
+        fake_file_path = 'data/my_cube.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 6)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 24)
+
+    def test_area(self):
+        self.polyedr.result()
+        self.assertAlmostEqual(self.polyedr.area, 1.0)
+
+
+class TestPolyedr3(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """40.0	45.0	-30.0	-60.0
+8	2	8
+1.0 8.0 0.0
+20.0 8.0 0.0
+20.0 11.0 0.0
+1.0 11.0 0.0
+6.0 6.0 8.0
+11.0 6.0 8.0
+11.0 11.0 8.0
+6.0 11.0 8.0
+4	1    2    3    4
+4	5    6    7    8"""
+        fake_file_path = 'data/planes.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 2)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 8)
+
+    def test_area(self):
+        self.polyedr.result()
+        self.assertAlmostEqual(self.polyedr.area, 25.0)
+
+
+class TestPolyedr4(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """10.0	45.0	-30.0	-60.0
+10	2	10
+1.0 8.0 0.0
+20.0 8.0 0.0
+20.0 11.0 0.0
+1.0 11.0 0.0
+6.0 6.0 8.0
+11.0 6.0 8.0
+13.0 7.0 8.0
+11.0 11.0 8.0
+6.0 11.0 8.0
+5.0 8.0 8.0
+4	1    2    3    4
+6	5    6    7    8    9    10"""
+        fake_file_path = 'data/planes2.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 10)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 2)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 10)
+
+    def test_area(self):
+        self.polyedr.result()
+        self.assertAlmostEqual(self.polyedr.area, 32.5)
+
+
+class TestPolyedr5(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """200.0	60.0	-140.0	60.0
+8	5	20
+-0.5	-0.5	0.5
+-0.5	0.5	0.5
+0.5	0.5	0.5
+0.5	-0.5	0.5
+-0.5	-0.5	-0.5
+-0.5	0.5	-0.5
+0.5	0.5	-0.5
+0.5	-0.5	-0.5
+4	1    2    3    4
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5"""
+        fake_file_path = 'data/box.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 5)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 20)
+
+    def test_area(self):
+        self.polyedr.result()
+        self.assertAlmostEqual(self.polyedr.area, 0.0)
